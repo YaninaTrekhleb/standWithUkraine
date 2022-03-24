020754ad558fe7fbc0ebada9e2abc1f7552b2ddb
Yanina Trekhleb <yanisavchenko@gmail.com>
2022-03-24 19:56:08 +0100

    Add slide.

---
+++ main.js
@@ -0,0 +1,62 @@
+const slidersData = [
+  {
+    title: 'How can I help Ukraine?',
+    link: '',
+    description: 'Thank you for your compassion for Ukraine\'s pain. On this page you will find different foundations that need financial help. These include the Ukrainian army, humanitarian funds, or people who are now hosting refugees. Also at the end of this page you can find Instagram pages to follow the situation in real time',
+    backgroundImagePosition: 0, // percentage of the background image width, (i.e. 0.3 is 30%)
+  },
+  {
+    title: 'Help for the army',
+    link: 'https://bank.gov.ua/en/news/all/natsionalniy-bank-vidkriv-spetsrahunok-dlya-zboru-koshtiv-na-potrebi-armiyi',
+    description: 'National Bank of Ukraine',
+    backgroundImagePosition: 0.2,
+  },
+  {
+    title: 'Help for the army',
+    link: 'https://www.google.com/',
+    description: 'https://www.comebackalive.in.ua/',
+    backgroundImagePosition: 0.3,
+  },
+  {
+    title: '',
+    link: '',
+    description: '',
+    backgroundImagePosition: 0.4,
+  },
+  {
+    title: '',
+    link: '',
+    description: '',
+    backgroundImagePosition: 0.5,
+  },
+  {
+    title: '',
+    link: '',
+    description: '',
+    backgroundImagePosition: 0.6,
+  },
+  {
+    title: '',
+    link: '',
+    description: '',
+    backgroundImagePosition: 0.7,
+  },
+  {
+    title: '',
+    link: '',
+    description: '',
+    backgroundImagePosition: 0.8,
+  },
+  {
+    title: '',
+    link: '',
+    description: '',
+    backgroundImagePosition: 0.8,
+  },
+  {
+    title: '',
+    link: '',
+    description: '',
+    backgroundImagePosition: 0.9,
+  }
+];
